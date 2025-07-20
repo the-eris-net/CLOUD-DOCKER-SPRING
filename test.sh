4826bd4b8d638504185860738aae4dd749e5040c
the-eris-net <the.eris.net@gmail.com>
2025-07-21 04:06:33 +0900

    feat: commit

---
+++ test.sh
@@ -0,0 +1,18 @@
+docker login
+#docker tag local-image:tagname new-repo:tagname
+docker tag goorm-docker-spring:latest theerisnet/goorm-docker-spring:latest
+docker push theerisnet/goorm-docker-spring:latest
+
+docker ps
+
+kubectl logs -f my-pod
+
+kubectl exec -it my-pod /bin/bash
+kubectl delete pod my-pod
+kubectl get events
+#kubectl port-forward pod/<pod-name> <local-port>:<container-port>
+kubectl port-forward pod/app 8888:8080
+
+kubectl edit rs my-pod-rs
+
+
